e5499d65a0fa7de08b2f550c5beaacf409eff927
R-Cunningham <42849450+R-Cunningham@users.noreply.github.com>
2018-09-03 15:38:21 -0500

    Add files via upload

--- 1.cs
+++ 1.cs
@@ -4,7 +4,7 @@ using System.Numerics;
 using System.Linq;
 
 
-//assume 64 points for original complex signal.
+
 static class Constants
 {
     public const double Pi = 3.14159265358;
@@ -15,61 +15,79 @@ public class Example
     {
     
         //This represents a signal of 64 samples, assume sample/s.
-        int Ss = 64;
-        Signal<double> sig = new Signal<double>(Ss);
-        Console.WriteLine("Signal SampleSize: {0}", sig.SS);
-        Console.WriteLine("Signal Amplitude: {0}", sig.A.GetHashCode());
-        sig.Freqs.Add(300);
-        sig.Freqs.Add(600);
-       
-        for (var i = 0; i < sig.Freqs.Count; i++)
+        int Ti = 16;
+        Signal<double> sig = new Signal<double>(Ti);
+        Console.WriteLine("Signal Sampling Period(pending): {0}", sig.Ti);
+        sig.FreqAmp.Add(new Tuple<double, double>(45, 1));
+        sig.FreqAmp.Add(new Tuple<double, double>(99, 1.5));
+        sig.FreqAmp.Add(new Tuple<double, double>(160, 1));
+        Console.WriteLine("Frequency: {0}", sig.FreqAmp[0].Item1);
+        Console.WriteLine("Amplitude: {0}", sig.FreqAmp[0].Item2);
+        int Fs = 360;
+        sig.InitializeSine(Fs);
+        for (var i = 0; i < sig.Output.Count; i++)
         {
-            Console.WriteLine("Frequency is {0}", sig.Freqs.ElementAt(i));
+            Console.WriteLine("Amplitude: {0} at sampled time: {1}", sig.Output.ElementAt(i), i * sig.MaxFreq(sig.FreqAmp)/Fs);
         }
-        sig.InitializeSine();
+
     }
 }
 
 public class Signal<Double>
 {
-    public List<double> Freqs;
+    public List<Tuple<double, double>> FreqAmp;
     public List<double> Output;
-    public int SS;
-    public int A { get; set; }
-    public Signal(int SampleSize) {
-        this.SS = SampleSize;   
-        this.A = 1;
-        this.Freqs= new List<double>();
-        this.Output = new List<double>();
+    public int Ti;
+    public int Fs { get; set; }
+    public Signal(int Time) {
+        Ti = Time;
+        Output = new List<double>();
+        FreqAmp = new List<Tuple<double, double>>();
     }
-    public List<double> InitializeSine()
-    {   //do math and pray.   
+    public List<double> InitializeSine(int SamplingFreq)
+    {     
         double value = 0;
-        for (int i = 1; i < SS; i++)
+        Fs = SamplingFreq;
+        double Fm = MaxFreq(FreqAmp);
+        for (float i = 0; i < Ti; i = i + ((float)Fm / Fs))
         {
-            for (int j = 0; j < Freqs.Count; j++)
-            {
-               value = value +  A * Math.Sin((2 * Constants.Pi * Freqs.ElementAt(j) * i) / 180);
-               
+            value = 0;
+            for (int j = 0; j < FreqAmp.Count; j++)
+            {    
+                value = value + (FreqAmp[j].Item2) * Math.Sin((2 * Constants.Pi * FreqAmp[j].Item1 * i) / 180);        
             }
-            Console.WriteLine("Value is {0} at time {1}", value, i);
             Output.Add(value);
         }
         return Output;
     }
-    
-    
-    
-  
+
+    public double MaxFreq(List<Tuple<double, double>> List)
+    {
+        double Compare = 0;
+        foreach (Tuple<double, double> obj in List)
+        {
+            if (obj.Item1 > Compare)
+                Compare = obj.Item1;
+
+        }
+        return Compare;
+    }
 }
 
+  
 
 
 
 
 
 
-/*
+
+
+
+
+
+
+/* Example of System.Numerics, complex class.
     List<Complex> points = new List<Complex>();
     points.Add(new Complex(0, 0));
     points.Add(new Complex(2, 0));
@@ -100,55 +118,7 @@ public class Signal<Double>
     }
     */
 
-
-/*
-public class ListExtensions : IComparable<List<Complex>>
-{
-    public static T Max<T, TCompare>(this IEnumerable<T> collection, Func<T, TCompare> func) where TCompare : IComparable<TCompare>
-    {
-        T maxItem = default(T);
-        TCompare maxValue = default(TCompare);
-        foreach (var item in collection)
-        {
-            TCompare temp = func(item);
-            if (maxItem == null || temp.CompareTo(maxValue) > 0)
-            {
-                maxValue = temp;
-                maxItem = item;
-            }
-        }
-        return maxItem;
-    }
-}
-
-
-    public double mag;
-    public double Max(List<Complex> list)
-    {
-        if (list.Count == 0)
-            throw new InvalidOperationException("Empty list!");
-        double maxValue = double.MinValue;
-        foreach (Complex obj in list)
-        {
-            if (obj.Magnitude > maxValue)
-                maxValue = obj.Magnitude;
-            else;
-        }
-        return maxValue;
-    }
-   public class ListExtensions : IComparable<List<Complex>> 
-       private double mag;
-       double Min = double.MinValue;
-       public double MagT(Complex obj)
-       {
-           mag = obj.Magnitude;
-           return mag;
-
-       }
-       */
-
-
-
+    
 
 
 
